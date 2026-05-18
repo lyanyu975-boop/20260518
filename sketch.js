e0b6227189af811008bb61244dc06a47402b35f4
lyanyu975-boop <lyanyu975@gmail.com>
2026-05-18 10:42:23 +0800

    2

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let video;
-let handPose;
-let hands = [];
+let handpose;
+let predictions = [];
 
 let playerChoice = "";
 let aiChoice = "";
@@ -10,31 +10,39 @@ let winCount = 0;
 let loseCount = 0;
 
 let countdown = 3;
-let lastTime = 0;
+let lastSecond = 0;
 
-let gameState = "countdown";
-
-function preload() {
-  handPose = ml5.handPose();
-}
+let state = "countdown";
 
 function setup() {
 
   createCanvas(windowWidth, windowHeight);
 
+  // 攝影機
   video = createCapture(VIDEO);
+
   video.size(640, 480);
+
   video.hide();
 
-  handPose.detectStart(video, gotHands);
+  // Handpose
+  handpose = ml5.handpose(video, modelReady);
+
+  handpose.on("predict", results => {
+
+    predictions = results;
+
+  });
 
   textAlign(CENTER, CENTER);
 
-  lastTime = millis();
+  lastSecond = millis();
 }
 
-function gotHands(results) {
-  hands = results;
+function modelReady() {
+
+  console.log("Handpose ready");
+
 }
 
 function draw() {
@@ -45,199 +53,207 @@ function draw() {
 
   drawCamera();
 
-  drawTopText();
+  drawTexts();
 
-  drawScore();
-
-  drawGameState();
+  gameLogic();
 }
 
 function drawGameUI() {
 
-  // 外框
   fill("#5a189a");
+
   noStroke();
+
   rect(40, 40, width - 80, height - 80, 40);
 
-  // 遊戲機螢幕
   fill("#e0aaff");
+
   rect(80, 80, width - 160, height - 160, 30);
-
-  // 裝飾按鈕
-  fill("#240046");
-  circle(width - 120, height - 100, 50);
-
-  fill("#9d4edd");
-  circle(width - 190, height - 100, 50);
 }
 
 function drawCamera() {
 
   let camW = width * 0.6;
+
   let camH = height * 0.55;
 
   let camX = width / 2 - camW / 2;
+
   let camY = height / 2 - camH / 2;
 
   push();
 
   // 修正鏡像
   translate(camX + camW, camY);
+
   scale(-1, 1);
 
   image(video, 0, 0, camW, camH);
 
-  drawSkeleton(camW, camH);
+  drawHand(camW, camH);
 
   pop();
 }
 
-function drawSkeleton(camW, camH) {
+function drawHand(camW, camH) {
 
-  for (let hand of hands) {
+  if (predictions.length > 0) {
 
-    let keypoints = hand.keypoints;
+    let landmarks = predictions[0].landmarks;
 
-    // 骨架點
-    for (let i = 0; i < keypoints.length; i++) {
+    // 畫點
+    for (let i = 0; i < landmarks.length; i++) {
 
-      let x = map(keypoints[i].x, 0, video.width, 0, camW);
-      let y = map(keypoints[i].y, 0, video.height, 0, camH);
+      let x = map(
+        landmarks[i][0],
+        0,
+        video.width,
+        0,
+        camW
+      );
+
+      let y = map(
+        landmarks[i][1],
+        0,
+        video.height,
+        0,
+        camH
+      );
 
       fill("#ffea00");
+
       noStroke();
+
       circle(x, y, 12);
     }
 
     // 手指骨架
-    drawFinger(keypoints, [0,1,2,3,4], camW, camH);
-    drawFinger(keypoints, [5,6,7,8], camW, camH);
-    drawFinger(keypoints, [9,10,11,12], camW, camH);
-    drawFinger(keypoints, [13,14,15,16], camW, camH);
-    drawFinger(keypoints, [17,18,19,20], camW, camH);
+    drawFinger(landmarks, [0,1,2,3,4], camW, camH);
+    drawFinger(landmarks, [5,6,7,8], camW, camH);
+    drawFinger(landmarks, [9,10,11,12], camW, camH);
+    drawFinger(landmarks, [13,14,15,16], camW, camH);
+    drawFinger(landmarks, [17,18,19,20], camW, camH);
   }
 }
 
-function drawFinger(keypoints, points, camW, camH) {
+function drawFinger(landmarks, points, camW, camH) {
 
   stroke("#00f5d4");
+
   strokeWeight(5);
 
   for (let i = 0; i < points.length - 1; i++) {
 
-    let p1 = keypoints[points[i]];
-    let p2 = keypoints[points[i + 1]];
+    let p1 = landmarks[points[i]];
 
-    let x1 = map(p1.x, 0, video.width, 0, camW);
-    let y1 = map(p1.y, 0, video.height, 0, camH);
+    let p2 = landmarks[points[i + 1]];
 
-    let x2 = map(p2.x, 0, video.width, 0, camW);
-    let y2 = map(p2.y, 0, video.height, 0, camH);
+    let x1 = map(p1[0], 0, video.width, 0, camW);
+
+    let y1 = map(p1[1], 0, video.height, 0, camH);
+
+    let x2 = map(p2[0], 0, video.width, 0, camW);
+
+    let y2 = map(p2[1], 0, video.height, 0, camH);
 
     line(x1, y1, x2, y2);
   }
 }
 
-function drawTopText() {
-
-  fill("#240046");
-
-  textSize(26);
-
-  text(
-    "414730266 留妍瑜",
-    width / 2,
-    55
-  );
-}
-
-function drawScore() {
+function drawTexts() {
 
   fill("#240046");
 
   textSize(24);
 
+  text(
+    "414730266 留妍瑜",
+    width / 2,
+    50
+  );
+
   textAlign(RIGHT);
 
   text(
-    "WIN : " + winCount + "   LOSE : " + loseCount,
-    width - 100,
-    55
+    "WIN : " + winCount +
+    "   LOSE : " + loseCount,
+    width - 80,
+    50
   );
 
   textAlign(CENTER);
-}
-
-function drawGameState() {
-
-  fill("#240046");
 
   textSize(60);
 
-  if (gameState == "countdown") {
+  if (state == "countdown") {
 
     text(countdown, width / 2, 120);
+  }
 
-    if (millis() - lastTime > 1000) {
+  else {
+
+    text("GO!", width / 2, 120);
+  }
+
+  textSize(28);
+
+  text(
+    "玩家 : " + playerChoice +
+    "      AI : " + aiChoice +
+    "      " + resultText,
+    width / 2,
+    height - 70
+  );
+}
+
+function gameLogic() {
+
+  if (state == "countdown") {
+
+    if (millis() - lastSecond > 1000) {
 
       countdown--;
 
-      lastTime = millis();
+      lastSecond = millis();
     }
 
     if (countdown < 0) {
 
-      gameState = "result";
-
       judgeGame();
+
+      state = "result";
+
+      setTimeout(resetGame, 2500);
     }
-  }
-
-  else if (gameState == "result") {
-
-    text("GO!", width / 2, 120);
-
-    fill("#3c096c");
-
-    textSize(28);
-
-    text(
-      "玩家 : " + playerChoice +
-      "      AI : " + aiChoice +
-      "      " + resultText,
-      width / 2,
-      height - 70
-    );
-
-    setTimeout(resetGame, 2500);
-
-    gameState = "waiting";
   }
 }
 
 function judgeGame() {
 
-  if (hands.length == 0) {
+  if (predictions.length == 0) {
 
     resultText = "沒有偵測到手";
 
     return;
   }
 
-  let keypoints = hands[0].keypoints;
+  let landmarks = predictions[0].landmarks;
 
-  let fingerCount = countFingers(keypoints);
+  let fingerCount = countFingers(landmarks);
 
-  // 玩家判定
+  // 玩家
   if (fingerCount == 0) {
+
     playerChoice = "石頭";
   }
 
   else if (fingerCount == 2) {
+
     playerChoice = "剪刀";
   }
 
   else if (fingerCount == 5) {
+
     playerChoice = "布";
   }
 
@@ -248,12 +264,12 @@ function judgeGame() {
     return;
   }
 
-  // AI隨機
+  // AI
   let choices = ["石頭", "剪刀", "布"];
 
   aiChoice = random(choices);
 
-  // 勝負判定
+  // 勝負
   if (playerChoice == aiChoice) {
 
     resultText = "平手";
@@ -278,24 +294,19 @@ function judgeGame() {
   }
 }
 
-function countFingers(keypoints) {
+function countFingers(landmarks) {
 
   let count = 0;
 
-  // 食指
-  if (keypoints[8].y < keypoints[6].y) count++;
+  if (landmarks[8][1] < landmarks[6][1]) count++;
 
-  // 中指
-  if (keypoints[12].y < keypoints[10].y) count++;
+  if (landmarks[12][1] < landmarks[10][1]) count++;
 
-  // 無名指
-  if (keypoints[16].y < keypoints[14].y) count++;
+  if (landmarks[16][1] < landmarks[14][1]) count++;
 
-  // 小指
-  if (keypoints[20].y < keypoints[18].y) count++;
+  if (landmarks[20][1] < landmarks[18][1]) count++;
 
-  // 拇指
-  if (keypoints[4].x < keypoints[3].x) count++;
+  if (landmarks[4][0] < landmarks[3][0]) count++;
 
   return count;
 }
@@ -304,9 +315,9 @@ function resetGame() {
 
   countdown = 3;
 
-  lastTime = millis();
+  lastSecond = millis();
 
-  gameState = "countdown";
+  state = "countdown";
 }
 
 function windowResized() {
